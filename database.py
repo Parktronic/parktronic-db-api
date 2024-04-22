9e0122f5e66a4275f2d577962249cab251dca143
Lisa Rozhkova <liza.roz@yandex.ru>
2024-04-22 14:04:27 +0300

    Move variables to .env

--- database.py
+++ database.py
@@ -1,8 +1,19 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/parktronic"
+load_dotenv()
+
+DB_HOST: str = os.getenv("DB_HOST")
+DB_PORT: int = os.getenv("DB_PORT")
+DB_USER: str = os.getenv("DB_USER")
+DB_PASS: str = os.getenv("DB_PASS")
+DB_NAME: str = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
